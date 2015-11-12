89826fa5435f08e1750fc80e82179c5075f29251
Tim Hyson <tjhyson@gmail.com>
2015-11-12 18:13:09 +0000

    User Story 7
    As an air traffic controller
    So the system is consistent and correctly reports plane status and location
    I want to ensure a flying plane cannot take off and cannot be in an airport

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,3 +1,10 @@
 class Plane
 
+  def take_off
+    raise 'Plane cannot take off: plane already flying.'
+  end
+
+  def airport
+    raise 'Plane cannot be in an airport: plane already flying.'
+  end
 end
